3730c37d046d6650c6c843a37301c05d33cdb685
syler921 <syler921@gmail.com>
2021-01-09 21:01:23 +0200

    Image edit in post edit

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
+const multer = require("multer");
+
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+      console.log("id ???",uuidv4())
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+      cb(null,true);
+  }
+  else { 
+      cb(null,false)
+  }
+};
+
 //app.use(bodyParser.urlencoded())
 app.use(bodyParser.json());
-app.use('/images',express.static(path.join(__dirname,'images')))
+app.use(
+    multer({storage:fileStorage, fileFilter:fileFilter}).single('image')
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -22,13 +51,13 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.use((error, req, res, next ) => { 
-    console.log(error);
-    const status = error.statusCode;
-    const message = error.message;
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode;
+  const message = error.message;
 
-    res.status(status).json({message:message})
-})
+  res.status(status).json({ message: message });
+});
 const MONGODB_URI =
   "mongodb+srv://syler:159753258@cluster0.okpks.mongodb.net/messages";
 mongoose
@@ -39,4 +68,3 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
